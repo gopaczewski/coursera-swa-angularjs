8d591bbd80509c60bfa04cc39090e513cc5576b6
Greg Opaczewski <gopaczewski@Gregs-MacBook-Air.local>
2020-12-22 10:27:56 -0600

    Module 1 Solution - Bonus

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -9,16 +9,20 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
     $scope.items = '';
+    $scope.messagefont = 'invalid-input';
     $scope.check = function() {   
         itemsArr = $scope.items.split(",")
             .filter(x => x.trim().length > 0);
 
         if (itemsArr.length > 3) {
             $scope.message = 'Too much!';
+            $scope.messagefont = 'valid-input';
         } else if (itemsArr.length > 0) {
             $scope.message = 'Enjoy!';
+            $scope.messagefont = 'valid-input';
         } else {
-            $scope.message = 'Please enter data first';;
+            $scope.message = 'Please enter data first';
+            $scope.messagefont = 'invalid-input';
         }
     }
 }
